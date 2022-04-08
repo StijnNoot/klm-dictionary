93267e536715f3176de263f5aa5e6078888c322e
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2022-04-08 13:44:47 +0200

    Fix versioning

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ source .env
 
 last_edited_date=$(date -r KLM/dict.xdxf '+%d-%m-%Y')
 last_edited_date_full=$(date -r KLM/dict.xdxf -u)
-VER_DESCRIBE=$(git describe --tags --dirty --long --match 'v[0-9]*' | sed 's/-/ /' | sed 's/^v//')
-VERSION=v$(($(echo "$VER_DESCRIBE" | cut -d' ' -f 1) + 1))+$(echo "$VER_DESCRIBE" | cut -d' ' -f 2)
+VERSION=$(git describe --tags --dirty --long --match 'v[0-9]*' | sed 's/-/+./')
 
 rm -rf Dictionary
 "${PYGLOSSARY_BIN-.venv/bin}/pyglossary" \
